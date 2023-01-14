70a64a5da9a1f77b6627ca9f92cc2b3f8b374f61
cipgiorgio <cip.giorgio@gmail.com>
2023-01-14 11:30:41 +0100

    Ok commit iniziale

---
+++ Solution/Result.cs
@@ -0,0 +1,52 @@
+﻿namespace Solution
+{
+    class Result
+    {
+
+        /*
+         * Complete the 'findSubstring' function below.
+         *
+         * The function is expected to return a STRING.
+         * The function accepts following parameters:
+         *  1. STRING s
+         *  2. INTEGER k
+         */
+
+        public static string findSubstring(string s, int k)
+        {
+            string vowels = "aeiou";
+            string result = "Not found!";
+            int imax=0;
+            int idx = -1;
+            string ss;
+
+            for (int i=0; i<=s.Length-k; i++)
+            {
+                ss = s.Substring(i, k);
+                //if (ss.Length >= k)
+                //{
+                    var max = 0;
+                    foreach (var item in ss)
+                    {
+                        if (vowels.Contains(item))
+                        {
+                            max++;
+                        }
+                    }
+                    if (imax < max)
+                    {
+                        imax = max;
+                        idx = i;
+                    }
+                //}
+            }
+            if (idx != -1)
+                result = s.Substring(idx,k);
+            return result;
+
+
+
+        }
+
+    }
+}
